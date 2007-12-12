694b14eea8a839cac2cd70c99c09fd987b267475
smoe <smoe@3e778353-6f3f-0410-9ba2-8ef0519b21f3>
2007-12-12 09:26:48 +0000

    Tinaa now works.
    git-svn-id: https://clcb.svn.sourceforge.net/svnroot/clcb/trunk@44 3e778353-6f3f-0410-9ba2-8ef0519b21f3

--- make-doc.lisp
+++ make-doc.lisp
@@ -1,6 +1,24 @@
-(in-package #'tinaa)
-(tinaa:document-system :package 'cbcl
-                       (pathname-directory *default-pathname-defaults*)
-		       :write-files? t
-		       :show-parts-without-documentation? nil
-)
+;(in-package :clcb-ensembl)
+;(tinaa:document-system :package *package*
+;                       "html/"
+;		       :write-files? t
+;		       :show-parts-without-documentation? nil
+;)
+;(in-package :clcb-utils)
+;(tinaa:document-system :package *package*
+;                       "html/"
+;		       :write-files? t
+;		       :show-parts-without-documentation? nil
+;)
+;(in-package :clcb)
+;(tinaa:document-system :package *package*
+;                       "html/"
+;		       :write-files? t
+;		       :show-parts-without-documentation? nil
+;)
+
+(tinaa:document-system :asdf-system 'clcb
+                       "html/" )
+
+;                       (pathname-directory *default-pathname-defaults*)
+
