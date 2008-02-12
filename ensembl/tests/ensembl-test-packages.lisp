ab3b1a7b6aeb5047297fc6083d5c7898c4cb5736
bleifreidenker <bleifreidenker@3e778353-6f3f-0410-9ba2-8ef0519b21f3>
2008-02-12 16:07:02 +0000

    Add test suites using LIFT.
    git-svn-id: https://clcb.svn.sourceforge.net/svnroot/clcb/trunk@84 3e778353-6f3f-0410-9ba2-8ef0519b21f3

---
+++ ensembl/tests/ensembl-test-packages.lisp
@@ -0,0 +1,7 @@
+(in-package #:common-lisp-user)
+
+(defpackage clcb-ensembl-test
+  (:use #:common-lisp
+        #:clcb
+        #:clcb-ensembl
+        #:lift))
---
+++ tests/utils-test.lisp
@@ -0,0 +1,26 @@
+(defpackage clcb-tests
+  (:use #:common-lisp
+        #:interval
+        #:lift))
+
+(in-package #:clcb-tests)
+
+(deftestsuite interval-tests () ())
+
+(addtest (interval-tests)
+  trivial-interval-reader-macro
+  (ensure-same #[3.1415,42]
+               (make-instance 'interval:trivial-interval
+                              :lower 3.1415
+                              :upper 42)
+               :test #'interval-=))
+
+(addtest (interval-tests)
+  integer-interval-reader-macro
+  (ensure-same #[23,42]
+               (make-instance 'interval:integer-interval :lower 23 :upper 42)
+               :test #'interval-=))
+
+
+
+
